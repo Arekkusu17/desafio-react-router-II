35853f20b5ea74b7fbe907e98ebd75ad8c9bc4d3
Arekkusu17 <fernn.alex@gmail.com>
2023-03-09 14:20:42 -0300

    change img for a gif

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,13 +1,15 @@
 import "../assets/css/home.css"
+import imageGif from "../assets/img/pokemon-img.gif"
 import backgroundImage from '../assets/img/pokemon-cards-bg.jpg'
 
 export default function Home() {
   return (
     <section>
-      <h1 className="home-title">
+      <h1 className="home-title mt-5">
         Bienvenido Maestro Pokémon
       </h1>
-      <img src={backgroundImage} className="img-fluid bg-image" alt=""></img>
+      <img src={imageGif} className="img-fluid bg-image mt-5" alt=""></img>
+      <p><a href="https://giphy.com/gifs/pokemon-tv-show-pokeball-j2xgBIuAgmrpS">via GIPHY</a></p>
     </section>
   )
 }
