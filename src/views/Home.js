ff6f03fa00f3b3ec93c26e9c2405320ecb5b8565
Arekkusu17 <fernn.alex@gmail.com>
2023-03-09 15:09:22 -0300

    fix design

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,17 +1,19 @@
 import "../assets/css/home.css"
 import imageGif from "../assets/img/pokemon-img.gif"
-import backgroundImage from '../assets/img/pokemon-cards-bg.jpg'
 
 export default function Home() {
   return (
     <>
-      <section>
-        <h1 className="home-title pt-2">
-          Bienvenido Maestro Pokémon
-        </h1>
-        <img src={imageGif} className="img-fluid bg-image mt-5" alt=""></img>
-        <p><a href="https://giphy.com/gifs/pokemon-tv-show-pokeball-j2xgBIuAgmrpS">via GIPHY</a></p>
+      <section className="home-view-section">
+        <div className="container d-flex flex-column align-items-center pt-5">
+          <h1 className="home-title">
+            Bienvenido Maestro Pokémon
+          </h1>
+          <img src={imageGif} className="img-fluid bg-image mt-5" alt=""></img>
+          <p><a href="https://giphy.com/gifs/pokemon-tv-show-pokeball-j2xgBIuAgmrpS">via GIPHY</a></p>
+        </div>
       </section>
+
     </>
   )
 }
