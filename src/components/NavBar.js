2a8d9d0c336fdb089468d20831c5d3479d01132e
Arekkusu17 <fernn.alex@gmail.com>
2023-03-09 15:36:31 -0300

    remove and clean files

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,6 @@ export default function NavBar() {
   return (
     <nav className="navbar navbar-expand-lg bg-light">
       <div className="container-fluid ">
-        {/* IMAGEN  class name order-lg-3*/}
         <NavLink to='/' className={'text-decoration-none'}><span className="is-error">pokeDex!</span></NavLink>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
--- src/views/PokemonFeed.js
+++ src/views/PokemonFeed.js
@@ -4,7 +4,6 @@ import React, { useEffect, useState } from 'react'
 import axios from "axios";
 
 import { useNavigate } from 'react-router-dom'
-import { PokemonContext } from '../context/PokemonProvider'
 
 
 export default function PokemonFeed() {
